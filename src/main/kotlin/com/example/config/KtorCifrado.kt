fbd4c94b61977a6e5631e8dbfd79a64f14a3d3e8
Manuel Garcia <m.gd@outlook.es>
2023-12-14 18:18:52 +0100

    Cifrado cambiado a MD5

--- src/main/kotlin/com/example/config/KtorCifrado.kt
+++ src/main/kotlin/com/example/config/KtorCifrado.kt
@@ -8,9 +8,9 @@ import javax.crypto.Cipher
 class KtorCifrado {
     companion object {
         fun cifrarContrasena(contrasena: String): String {
-            val sha256 = MessageDigest.getInstance("SHA-256")
+            val MD5 = MessageDigest.getInstance("MD5")
             val contrasenaBytes = contrasena.toByteArray(Charsets.UTF_8)
-            val contrasenaCifradaBytes = sha256.digest(contrasenaBytes)
+            val contrasenaCifradaBytes = MD5.digest(contrasenaBytes)
 
             return contrasenaCifradaBytes.joinToString("") {
                 "%02x".format(it)
@@ -18,8 +18,8 @@ class KtorCifrado {
         }
 
         fun descifrarContrasena(contrasenaCifrada: String): String {
-            val cifrador = Cipher.getInstance("SHA-256")
-            val clave = SecretKeySpec(contrasenaCifrada.toByteArray(Charsets.UTF_8), "SHA-256")
+            val cifrador = Cipher.getInstance("MD5")
+            val clave = SecretKeySpec(contrasenaCifrada.toByteArray(Charsets.UTF_8), "MD5")
             cifrador.init(Cipher.DECRYPT_MODE, clave)
 
             val contrasenaBytes = cifrador.doFinal(Base64.getDecoder().decode(contrasenaCifrada))
