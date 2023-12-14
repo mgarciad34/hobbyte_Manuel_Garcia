0f71130a85c2e01011407db223ab46a901744618
Manuel Garcia <m.gd@outlook.es>
2023-12-14 18:15:00 +0100

    Añadida funcionalidad Descifrar Contraseña

--- src/main/kotlin/com/example/config/KtorCifrado.kt
+++ src/main/kotlin/com/example/config/KtorCifrado.kt
@@ -1,6 +1,10 @@
 package com.example.config
 
 import java.security.MessageDigest
+import javax.crypto.spec.SecretKeySpec
+import java.util.Base64
+import javax.crypto.Cipher
+
 class KtorCifrado {
     companion object {
         fun cifrarContrasena(contrasena: String): String {
@@ -12,5 +16,15 @@ class KtorCifrado {
                 "%02x".format(it)
             }
         }
+
+        fun descifrarContrasena(contrasenaCifrada: String): String {
+            val cifrador = Cipher.getInstance("SHA-256")
+            val clave = SecretKeySpec(contrasenaCifrada.toByteArray(Charsets.UTF_8), "SHA-256")
+            cifrador.init(Cipher.DECRYPT_MODE, clave)
+
+            val contrasenaBytes = cifrador.doFinal(Base64.getDecoder().decode(contrasenaCifrada))
+            return String(contrasenaBytes, Charsets.UTF_8)
+        }
+
     }
 }
